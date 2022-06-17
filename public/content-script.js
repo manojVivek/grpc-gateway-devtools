0aed47aec7fc70d9c675f7abc3594ae3354c30eb
Manoj Vivek <p.manoj.vivek@gmail.com>
2022-06-17 16:59:50 +0530

    Content script modified to capture GRPC gateway requests

--- public/content-script.js
+++ public/content-script.js
@@ -1,110 +1,60 @@
 // Copyright (c) 2019 SafetyCulture Pty Ltd. All Rights Reserved.
 
 const injectContent = `
-window.__GRPCWEB_DEVTOOLS__ = function (clients) {
+window.__GRPCWEB_DEVTOOLS__ = function (clients, UnaryCall) {
   if (clients.constructor !== Array) {
     return
   }
   const postType = "__GRPCWEB_DEVTOOLS__";
-  var StreamInterceptor = function (method, request, stream) {
-    this._callbacks = {};
-    const methodType = "server_streaming";
-    window.postMessage({
-      type: postType,
-      method,
-      methodType,
-      request: request.toObject(),
-    });
-    stream.on('data', response => {
-      window.postMessage({
-        type: postType,
-        method,
-        methodType,
-        response: response.toObject(),
+  const interceptor = {
+    interceptUnary: (next, method, input, options) => {
+      const callPromise = new Promise(resolve => {
+        resolve(next(method, input, options));
       });
-      if (!!this._callbacks['data']) {
-        this._callbacks['data'](response);
-      }
-    });
-    stream.on('status', status => {
-      if (status.code === 0) {
-        window.postMessage({
-          type: postType,
-          method,
-          methodType,
-          response: "EOF",
-        });
-      }
-      if (!!this._callbacks['status']) {
-        this._callbacks['status'](status);
-      }
-    });
-    stream.on('error', error => {
-      if (error.code !== 0) {
-        window.postMessage({
-          type: postType,
-          method,
-          methodType,
-          error: {
-            code: error.code,
-            message: error.message,
-          },
-        });
-      }
-      if (!!this._callbacks['error']) {
-        this._callbacks['error'](error);
-      }
-    });
-    this._stream = stream;
-  }
-  StreamInterceptor.prototype.on = function (type, callback) {
-    this._callbacks[type] = callback;
-    return this;
-  }
-  StreamInterceptor.prototype.cancel = function () {
-    this._stream.cancel()
-  }
-  clients.map(client => {
-    client.client_.rpcCall_ = client.client_.rpcCall;
-    client.client_.rpcCall2 = function (method, request, metadata, methodInfo, callback) {
-      var posted = false;
-      var newCallback = function (err, response) {
-        if (!posted) {
+      return new UnaryCall(
+        method,
+        options.meta ?? {},
+        input,
+        callPromise.then(c => c.headers).catch(err => {}),
+        callPromise.then(c => {
+          console.log('Resolving', c.status);
           window.postMessage({
             type: postType,
-            method,
+            method: method.service.typeName+'.'+method.name,
             methodType: "unary",
-            request: request.toObject(),
-            response: err ? undefined : response.toObject(),
-            error: err || undefined,
+            request: input,
+            response: c.response,
+            error: undefined,
           }, "*")
-          posted = true;
-        }
-        callback(err, response)
-      }
-      return this.rpcCall_(method, request, metadata, methodInfo, newCallback);
+          return c.response;
+        }).catch(err => {
+          window.postMessage({
+            type: postType,
+            method: method.service.typeName+'.'+method.name,
+            methodType: "unary",
+            request: input,
+            response: undefined,
+            error: err.message,
+          }, "*")
+        }),
+        callPromise.then(c => c.status).catch(err => {}),
+        callPromise.then(c => c.trailers).catch(err => {})
+      );
+    },
+  };
+
+  clients.forEach(client => {
+    const interceptors = (client._transport.defaultOptions.interceptors || [])
+    if (interceptors.includes(interceptor)) {
+      return;
     }
-    client.client_.rpcCall = client.client_.rpcCall2;
-    client.client_.unaryCall = function (method, request, metadata, methodInfo) {
-      return new Promise((resolve, reject) => {
-        this.rpcCall2(method, request, metadata, methodInfo, function (error, response) {
-          error ? reject(error) : resolve(response);
-        });
-      });
-    };
-    client.client_.serverStreaming_ = client.client_.serverStreaming;
-    client.client_.serverStreaming2 = function (method, request, metadata, methodInfo) {
-      var stream = client.client_.serverStreaming_(method, request, metadata, methodInfo);
-      var si = new StreamInterceptor(method, request, stream);
-      return si;
-    }
-    client.client_.serverStreaming = client.client_.serverStreaming2;
+    client._transport.defaultOptions.interceptors = [...interceptors, interceptor];
   })
 }
-`
+`;
 
-let s = document.createElement('script');
-s.type = 'text/javascript';
+let s = document.createElement("script");
+s.type = "text/javascript";
 const scriptNode = document.createTextNode(injectContent);
 s.appendChild(scriptNode);
 (document.head || document.documentElement).appendChild(s);
